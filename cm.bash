af38519a01312f83c408e16ae6c2427d36882830
xy <wudidadaqiuqiu@qq.com>
2025-01-07 01:11:42 +0800

    add submodule rqt_signal_publisher

--- cm.bash
+++ cm.bash
@@ -1,1 +1,2 @@
-colcon build --symlink-install --packages-select robot_msg connector
+colcon build --symlink-install --packages-select robot_msg connector control_evaluate
+source install/setup.bash
---
+++ topic_pub.bash
@@ -0,0 +1,3 @@
+ros2 topic pub /signal_gen_cmd std_msgs/msg/Float32MultiArray "{
+    data: [1]
+}"
