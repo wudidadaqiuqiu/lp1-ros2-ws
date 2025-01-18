a77f2c83f7a83d0be03f593029fe2be4948bb98f
xy <wudidadaqiuqiu@qq.com>
2025-01-18 18:50:05 +0800

    add clangd

--- cm.bash
+++ cm.bash
@@ -1,2 +1,2 @@
-colcon build --symlink-install --packages-select robot_msg connector control_evaluate rqt_signal_publisher
+colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --symlink-install --packages-select robot_msg connector control_evaluate rqt_signal_publisher
 source install/setup.bash
