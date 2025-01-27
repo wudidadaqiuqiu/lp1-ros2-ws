641cbf70931095552ad21a724db7c4d0733c053a
xy <wudidadaqiuqiu@qq.com>
2025-01-28 00:17:31 +0800

    param interface:easy to declare and set params in observer and controller layer

--- cm.bash
+++ cm.bash
@@ -1,2 +1,2 @@
-colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --symlink-install --packages-select robot_msg connector control_evaluate rqt_signal_publisher
+colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --symlink-install --packages-select robot_msg connector control_evaluate rqt_signal_publisher rqt_reconfigure
 source install/setup.bash
---
+++ ros2run.bash
@@ -0,0 +1,2 @@
+n=$1
+ros2 run connector omni3 --ros-args --params-file $n
