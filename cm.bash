0b2b99e0a593ee05731888f4716a51228a0b7650
xy <wudidadaqiuqiu@qq.com>
2025-03-12 21:34:26 +0800

    add opt_control pkg

--- cm.bash
+++ cm.bash
@@ -1,2 +1,2 @@
-colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --symlink-install --packages-select robot_msg connector control_evaluate rqt_signal_publisher rqt_reconfigure
+colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --symlink-install --packages-select robot_msg connector control_evaluate rqt_signal_publisher rqt_reconfigure opt_control
 source install/setup.bash
